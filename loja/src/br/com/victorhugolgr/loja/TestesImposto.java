37f7735435cf5869eb6dfb55678668b6aaf273c5
victor.hugo <victor.hugo@tre-pa.jus.br>
2021-01-05 16:01:17 -0300

    Decorator

--- loja/src/br/com/victorhugolgr/loja/TestesImposto.java
+++ loja/src/br/com/victorhugolgr/loja/TestesImposto.java
@@ -3,19 +3,16 @@ package br.com.victorhugolgr.loja;
 import java.math.BigDecimal;
 
 import br.com.victorhugolgr.loja.imposto.CalculadoraDeImpostos;
-import br.com.victorhugolgr.loja.imposto.ICMS;
 import br.com.victorhugolgr.loja.imposto.ISS;
 import br.com.victorhugolgr.loja.orcamento.Orcamento;
 
-public class TesteImpostos {
+public class TestesImposto {
 
 	public static void main(String[] args) {
-		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 2);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 		
-		System.out.println(calculadora.calcular(orcamento, new ICMS(null)));
-		System.out.println(calculadora.calcular(orcamento, new ISS(null)));
-
+		System.out.println(calculadora.calcular(orcamento, new ISS(new ISS(null))));
+		
 	}
-
 }
